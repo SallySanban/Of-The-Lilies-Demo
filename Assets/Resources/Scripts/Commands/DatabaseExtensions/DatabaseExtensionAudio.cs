804ca6254ed043c6a8b105c8a3c47225a06b9836
antoinelabuanan <antoine.labuanan@gmail.com>
2025-01-26 10:05:18 +0800

    optimized audio codes; playing and stopping events now works properly during testing

--- Assets/Resources/Scripts/Commands/DatabaseExtensions/DatabaseExtensionAudio.cs
+++ Assets/Resources/Scripts/Commands/DatabaseExtensions/DatabaseExtensionAudio.cs
@@ -9,74 +9,78 @@ namespace Commands
 {
     public class DatabaseExtensionAudio : CommandDatabaseExtension
     {
-        private static List<EventInstance> activeEvents = new List<EventInstance>();
-        private static int eventCounter = 0;
+        private static Dictionary<string, EventInstance> activeEvents = new Dictionary<string, EventInstance>();
         private const string MUSIC_FILEPATH = "event:/Music/";
         private const string SFX_FILEPATH = "event:/SFX/";
         private const string AMB_FILEPATH = "event:/Ambience/";
-        
+
 
         new public static void Extend(CommandDatabase database)
         {
-            
-            database.AddCommand("playMusic", new Action<string>(playMusic));
+
             database.AddCommand("playSFX", new Action<string>(playSFX));
+            database.AddCommand("playMusic", new Action<string>(playMusic));
             database.AddCommand("playAmbience", new Action<string>(playAmbience));
             database.AddCommand("stopEvent", new Action<string>(stopEvent));
         }
 
-        private static void playMusic(string filename)
-        {
-            eventCounter++;
-            FMOD.Studio.EventInstance music;
-            music = FMODUnity.RuntimeManager.CreateInstance(MUSIC_FILEPATH + filename);
-            music.start();
-            Debug.Log("Playing Event " + eventCounter + ": " + MUSIC_FILEPATH + filename);
-            activeEvents.Add(music);
-        }
         private static void playSFX(string filename)
         {
             FMODUnity.RuntimeManager.PlayOneShot(SFX_FILEPATH + filename);
         }
 
-        private static void playAmbience(string filename)
+        private static void playMusic(string filename)
         {
-            eventCounter++;
-            FMOD.Studio.EventInstance amb;
-            amb = FMODUnity.RuntimeManager.CreateInstance(AMB_FILEPATH + filename);
-            amb.start();
-            Debug.Log("Playing Event " + eventCounter + ": " + AMB_FILEPATH + filename);
-            activeEvents.Add(amb);
+            string fullPath = MUSIC_FILEPATH + filename;
+            if (!activeEvents.ContainsKey(fullPath))
+            {
+                EventInstance music = RuntimeManager.CreateInstance(fullPath);
+                music.start();
+                activeEvents.Add(fullPath, music);
+                Debug.Log("Playing Music: " + fullPath);
+            }
+            else
+            {
+                Debug.LogWarning("Music is already playing: " + fullPath);
+            }
         }
 
-        private static void stopEvent(string data)
+        private static void playAmbience(string filename)
         {
-            if (int.TryParse(data, out int eventID))
+            string fullPath = AMB_FILEPATH + filename;
+            if (!activeEvents.ContainsKey(fullPath))
             {
-                if (eventID <= 0 || eventID > activeEvents.Count)
-                {
-                    Debug.Log($"Stopping Event with ID: {eventID}. Total active events: {activeEvents.Count}");
-                    return;
-                }
-
-                else
-                {
-                    FMOD.Studio.EventInstance eventToStop = activeEvents[eventID - 1];
-                    Debug.Log("Stopping Event ID " + eventID + " (Index: " + (eventID - 1) + ")");
-                    eventToStop.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-                    eventToStop.release();
-                    Debug.Log("Event " + eventID + " stopped successfully.");
-                }
+                EventInstance ambience = RuntimeManager.CreateInstance(fullPath);
+                ambience.start();
+                activeEvents.Add(fullPath, ambience);
+                Debug.Log("Playing Ambience: " + fullPath);
             }
-            //else
-            //{
-            //    FMOD.Studio.EventInstance eventToStop = activeEvents[eventID - 1];
-            //    Debug.Log("Stopping Event ID " + eventID + " (Index: " + (eventID - 1) + ")");
-            //    eventToStop.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-            //    eventToStop.release();
-            //    activeEvents.RemoveAt(eventID - 1);
-            //    Debug.Log("Event " + eventID + " stopped successfully.");
-            //}
+            else
+            {
+                Debug.LogWarning("Ambience is already playing: " + fullPath);
+            }
+        }
+
+        private static void stopEvent(string filename)
+        {
+            string fullPath = MUSIC_FILEPATH + filename;
+            if (!activeEvents.ContainsKey(fullPath))
+            {
+                fullPath = AMB_FILEPATH + filename; // Check if it's an ambience event
+            }
+
+            if (activeEvents.ContainsKey(fullPath))
+            {
+                EventInstance eventToStop = activeEvents[fullPath];
+                eventToStop.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+                eventToStop.release();
+                activeEvents.Remove(fullPath);
+                Debug.Log("Stopped Event: " + fullPath);
+            }
+            else
+            {
+                Debug.LogWarning("Event not found: " + filename);
+            }
         }
     }
 }
