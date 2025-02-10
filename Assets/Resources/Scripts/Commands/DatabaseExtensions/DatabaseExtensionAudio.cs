781d79fbb1f71a73b79503182f35e53bf17ffed1
antoinelabuanan <antoine.labuanan@gmail.com>
2025-02-10 17:26:41 +0800

    added new resume/stop audio event codes, added various bgm
    -added resume/pause command; currently pauses audio abruptly, will fix it tomorrow, feb 11
    -added kuchai town bgm, tavern bgm, sabina's salves bgm

--- Assets/Resources/Scripts/Commands/DatabaseExtensions/DatabaseExtensionAudio.cs
+++ Assets/Resources/Scripts/Commands/DatabaseExtensions/DatabaseExtensionAudio.cs
@@ -22,6 +22,8 @@ namespace Commands
             database.AddCommand("playMusic", new Action<string>(playMusic));
             database.AddCommand("playAmbience", new Action<string>(playAmbience));
             database.AddCommand("stopEvent", new Action<string>(stopEvent));
+            database.AddCommand("pauseEvent", new Action<string>(pauseEvent));
+            database.AddCommand("resumeEvent", new Action<string>(resumeEvent));
         }
 
         private static void playSFX(string filename)
@@ -76,6 +78,46 @@ namespace Commands
                 eventToStop.release();
                 activeEvents.Remove(fullPath);
                 Debug.Log("Stopped Event: " + fullPath);
+            }
+            else
+            {
+                Debug.LogWarning("Event not found: " + filename);
+            }
+        }
+
+        private static void pauseEvent(string filename)
+        {
+            string fullPath = MUSIC_FILEPATH + filename;
+            if (!activeEvents.ContainsKey(fullPath))
+            {
+                fullPath = AMB_FILEPATH + filename; // Check if it's an ambience event
+            }
+
+            if (activeEvents.ContainsKey(fullPath))
+            {
+                EventInstance eventToPause = activeEvents[fullPath];
+                eventToPause.setPaused(true);
+                Debug.Log("Paused Event: " + fullPath);
+            }
+            else
+            {
+                Debug.LogWarning("Event not found: " + filename);
+            }
+        }
+
+        private static void resumeEvent(string filename)
+        {
+            string fullPath = MUSIC_FILEPATH + filename;
+            if (!activeEvents.ContainsKey(fullPath))
+            {
+                fullPath = AMB_FILEPATH + filename; // Check if it's an ambience event
+            }
+
+            if (activeEvents.ContainsKey(fullPath))
+            {
+                EventInstance eventToResume = activeEvents[fullPath];
+                eventToResume.setPaused(false);
+                Debug.Log("Resumed Event: " + fullPath);
             }
             else
             {
